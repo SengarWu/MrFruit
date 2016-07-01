3375539c5d691d343c743d929f624c8c4e310659
Daisy_yuki <785930010@qq.com>
2016-07-01 10:13:46 +0800

    account修改

--- app/src/main/java/com/xpple/fruits/me/ui/AccountActivity.java
+++ app/src/main/java/com/xpple/fruits/me/ui/AccountActivity.java
@@ -1,17 +1,34 @@
 package com.xpple.fruits.me.ui;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.xpple.fruits.R;
+import com.xpple.fruits.base.BaseActivity;
 
-public class AccountActivity extends AppCompatActivity {
+public class AccountActivity extends BaseActivity {
+    private ImageButton ib_back;
+    private TextView tv_title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
 
+        initView();
+    }
 
+    private void initView() {
+        ib_back = $(R.id.ib_back);
+        ib_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        tv_title = $(R.id.tv_title);
+        tv_title.setText("结算");
     }
 }
