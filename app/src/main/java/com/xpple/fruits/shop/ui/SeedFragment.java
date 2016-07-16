5bafa928ccdf194442d2f4fd293277c94a73e5f3
sengar <76829913@qq.com>
2016-07-17 00:21:22 +0800

    界面更新

---
+++ app/src/main/java/com/xpple/fruits/shop/ui/SeedFragment.java
@@ -0,0 +1,19 @@
+package com.xpple.fruits.shop.ui;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.xpple.fruits.R;
+import com.xpple.fruits.base.BaseFragment;
+
+public class SeedFragment extends BaseFragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_seed, container, false);
+    }
+}
---
+++ app/src/main/java/com/xpple/fruits/shop/ui/ShoppingActivity.java
@@ -0,0 +1,45 @@
+package com.xpple.fruits.shop.ui;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import com.xpple.fruits.R;
+import com.xpple.fruits.base.BaseActivity;
+
+public class ShoppingActivity extends BaseActivity implements View.OnClickListener {
+
+    private ImageButton ib_back;
+    private TextView tv_title;
+    private RadioGroup rg_shop;
+    private FrameLayout fl_shop;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shopping);
+        initView();
+        tv_title.setText("水果专区");
+    }
+
+    private void initView() {
+        ib_back = $(R.id.ib_back);
+        ib_back.setOnClickListener(this);
+        tv_title = $(R.id.tv_title);
+        rg_shop = $(R.id.rg_shop);
+        fl_shop = $(R.id.fl_shop);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId())
+        {
+            case R.id.ib_back:
+
+                break;
+        }
+    }
+}
